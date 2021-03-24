917c6b63662d9c24c818ad0fade28ddf119994bb
RubenFern <rubenferval@hotmail.com>
2021-03-25 00:32:24 +0100

    Migración creada con las columnas de la tabla películas

---
+++ database/migrations/2021_03_24_215005_create_peliculas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeliculasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peliculas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_genero');
+            $table->string('titulo');
+            $table->string('director');
+            $table->integer('año');
+            $table->float('precio')->unsigned(); // FLoat por defecto usa 2 decimales
+            $table->longText('sinopsis');
+            $table->integer('cantidad')->unsigned();
+            $table->string('imagen')->default('not_found.png'); // Imagen por defecto en caso de no tenerla
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peliculas');
+    }
+}

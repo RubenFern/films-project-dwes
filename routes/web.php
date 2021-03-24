7de43d2e6381809c5f55921cee5aaa3b67f12551
RubenFern <rubenferval@hotmail.com>
2021-03-25 00:24:57 +0100

    Rutas del crud de los géneros añadidas. Tambíen se añadió una ruta para visualizar las películas alquiladas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\GeneroController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\PeliculaAlquiladaController;
 use App\Http\Controllers\PeliculaController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,13 +17,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
+Route::get('/', [MainController::class, 'index']);
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 /*
@@ -37,22 +36,33 @@ Route::post('peliculas', [PeliculaController::class, 'store'])->name('peliculas.
 Route::get('peliculas/{pelicula}', [PeliculaController::class, 'show'])->name('peliculas.show');
 
 /**** Rutas para editar películas ****/
-Route::get('peliculas/{pelicula}/editar', [PeliculaController::class, 'edit'])->name('peliculas.edit');
+Route::get('peliculas/{pelicula}/editar', [PeliculaController::class, 'edit'])->name('peliculas.edit'); // admin
 // Uso el método match porque para actualizar existen 2 métodos
-Route::match(['put', 'patch'], 'peliculas/{pelicula}', [PeliculaController::class, 'update'])->name('peliculas.update'); 
+Route::match(['put', 'patch'], 'peliculas/{pelicula}', [PeliculaController::class, 'update'])->name('peliculas.update'); // admin
 
 /**** Ruta para eliminar una película ****/
-Route::delete('peliculas/{pelicula}', [PeliculaController::class, 'destroy'])->name('peliculas.destroy');
+Route::delete('peliculas/{pelicula}', [PeliculaController::class, 'destroy'])->name('peliculas.destroy'); // admin
 
 /*
 |--------------------------------------------------------------------------
 |  Rutas de los Géneros
 |--------------------------------------------------------------------------
 */
+Route::get('generos', [GeneroController::class, 'index'])->name('generos.index');
 
+Route::get('generos/añadir', [GeneroController::class, 'create'])->name('generos.create'); // admin
+Route::post('generos', [GeneroController::class, 'store'])->name('generos.store'); // admin
+
+Route::get('generos/{genero}', [GeneroController::class, 'show'])->name('generos.show');
+
+Route::get('generos/{genero}/editar', [GeneroController::class, 'edit'])->name('generos.edit'); // admin
+Route::match(['put', 'patch'], 'generos/{genero}', [GeneroController::class, 'update'])->name('generos.update'); // admin
+
+Route::delete('generos/{genero}', [GeneroController::class, 'destroy'])->name('generos.destroy'); // admin
 
 /*
 |--------------------------------------------------------------------------
-|  Rutas para alquilar
+|  Ruta para visulizar las películas alquiladas
 |--------------------------------------------------------------------------
 */
+Route::get('peliculas-alquiladas', [PeliculaAlquiladaController::class, 'index'])->name('peliculas-alquiladas.index');

c855f4e25efb8ecce88791c970ff6d15e1817ed0
RubenFern <rubenferval@hotmail.com>
2021-03-25 00:23:54 +0100

    Retorno de las vistas en los métodos añadidas

--- app/Http/Controllers/PeliculaController.php
+++ app/Http/Controllers/PeliculaController.php
@@ -9,35 +9,30 @@ class PeliculaController extends Controller
     // Muestro todas las películas de la base de datos
     public function index()
     {
-        return "Muestro todas las películas";
-        //return view('home');
+        return view('peliculas/index');
     }
     // Muestro sólo la película que coincida con el id solicitado
     public function show($id)
     {
-        return "Muestro la película con el $id";
-        //return view('home');
+        return view('peliculas/show', compact('id'));
     }
 
     // Retono la vista del formulario para añadir una película
     public function create()
     {
-        return "Muestro el formulario para añadir una peli";
-        //return view('home');
+        return view('peliculas/create');
     }
     // La acción del formulario de añadir película redirige a este 
     // método para guardar los datos en la base de datos
     public function store($id)
     {
-        // retornaría la vista get por defecto 
         //
     }
 
     // Muestro el formulario para editar una película en específico
     public function edit($id)
     {
-        return "Muestro el form para editar una película con el id $id"; 
-        //return view('home');
+        return view('peliculas/edit', compact('id'));
     }
     // La acción del formulario de editar me redirige a este método para 
     // actualizar los datos en la base de datos

bf353013466e9a5778dcf94c2c15a14898e170b0
RubenFern <rubenferval@hotmail.com>
2021-03-24 19:04:51 +0100

    Añadidas todas las rutas de peliculas y llamadas a los métodos mediante peticiones GET, POST, PUT/PATCH y DELETE

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PeliculaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +18,41 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Auth::routes();
 
-Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
+/*
+|--------------------------------------------------------------------------
+|  Rutas de las Películas
+|--------------------------------------------------------------------------
+*/
+Route::get('peliculas', [PeliculaController::class, 'index'])->name('peliculas.index');
+
+/**** Rutas para añadir películas ****/
+Route::get('peliculas/añadir', [PeliculaController::class, 'create'])->name('peliculas.create'); // admin
+Route::post('peliculas', [PeliculaController::class, 'store'])->name('peliculas.store'); // admin
+
+Route::get('peliculas/{pelicula}', [PeliculaController::class, 'show'])->name('peliculas.show');
+
+/**** Rutas para editar películas ****/
+Route::get('peliculas/{pelicula}/editar', [PeliculaController::class, 'edit'])->name('peliculas.edit');
+// Uso el método match porque para actualizar existen 2 métodos
+Route::match(['put', 'patch'], 'peliculas/{pelicula}', [PeliculaController::class, 'update'])->name('peliculas.update'); 
+
+/**** Ruta para eliminar una película ****/
+Route::delete('peliculas/{pelicula}', [PeliculaController::class, 'destroy'])->name('peliculas.destroy');
+
+/*
+|--------------------------------------------------------------------------
+|  Rutas de los Géneros
+|--------------------------------------------------------------------------
+*/
+
+
+/*
+|--------------------------------------------------------------------------
+|  Rutas para alquilar
+|--------------------------------------------------------------------------
+*/

9d91247bc8cc18049dd64660b11c57ae04c345aa
RubenFern <rubenferval@hotmail.com>
2021-03-25 21:44:58 +0100

    Visualización de las películas en las vistas

--- resources/views/peliculas/index.blade.php
+++ resources/views/peliculas/index.blade.php
@@ -8,5 +8,21 @@
 </head>
 <body>
     <h2>Muestro todas las películas</h2>
+
+    <table border="1">
+        <tr>
+            <th>Título</th> <th>Sinopsis</th> <th>Director</th> <th>Año</th> <th>imagen</th>
+        </tr>
+        @foreach ($peliculas as $pelicula)
+            <tr>
+                <td><a href="peliculas/{{ $pelicula->id }}">{{ $pelicula->titulo }}</a></td> 
+                <td>{{ $pelicula->sinopsis }}</td> 
+                <td>{{ $pelicula->director }}</td> 
+                <td>{{ $pelicula->año }}</td> 
+                <td><img width="50" src="{{ $pelicula->imagen }}" ></td>
+            </tr>
+        @endforeach
+    </table>
+    
 </body>
 </html>
--- resources/views/peliculas/show.blade.php
+++ resources/views/peliculas/show.blade.php
@@ -7,6 +7,19 @@
     <title>Document</title>
 </head>
 <body>
-    <h2>Información de la película <?php echo $id; ?></h2>
+    <h2>Información de la película <?php echo $pelicula->id; ?></h2>
+
+    <table border="1">
+        <tr>
+            <th>Título</th> <th>Sinopsis</th> <th>Director</th> <th>Año</th> <th>imagen</th>
+        </tr>
+        <tr>
+            <td>{{ $pelicula->titulo }}</td> 
+            <td>{{ $pelicula->sinopsis }}</td> 
+            <td>{{ $pelicula->director }}</td> 
+            <td>{{ $pelicula->año }}</td> 
+            <td><img width="50" src="{{ $pelicula->imagen }}" ></td>
+        </tr>
+    </table>
 </body>
 </html>

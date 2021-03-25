3ad4bfc4fd79c520be0668b50f8bdb056d4a462f
RubenFern <rubenferval@hotmail.com>
2021-03-25 20:54:26 +0100

    Creado array para la asignación masiva de datos en los campos de la tabla y creado método para devolver el id del role según su nombre

---
+++ app/Models/Role.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'role'
+    ];
+
+    public static function getRoleByName(String $rolename)
+    {
+        return Role::select('id')->where('role', $rolename);
+    }
+}

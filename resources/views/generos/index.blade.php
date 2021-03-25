296fcf65a8ebdc8f5302476bed16744cdc343a1a
RubenFern <rubenferval@hotmail.com>
2021-03-26 00:56:19 +0100

    Ahora muestra todas las películas

--- resources/views/generos/index.blade.php
+++ resources/views/generos/index.blade.php
@@ -8,5 +8,16 @@
 </head>
 <body>
     <h2>Visualizo todos los generos</h2>
+
+    <table border="1">
+        <tr>
+            <th>Género</th>
+        </tr>
+        @foreach ($generos as $genero)
+            <tr>
+                <td><a href="generos/{{ $genero->id }}">{{ $genero->genero }}</a></td> 
+            </tr>
+        @endforeach
+    </table>
 </body>
 </html>

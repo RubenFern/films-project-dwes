72912fef0a7d75f9bf27411b1aa36c3baa096f73
RubenFern <rubenferval@hotmail.com>
2021-03-25 21:42:54 +0100

    Añadida consultas de Eloquent en el método index y show

--- app/Http/Controllers/PeliculaController.php
+++ app/Http/Controllers/PeliculaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pelicula;
 use Illuminate\Http\Request;
 
 class PeliculaController extends Controller
@@ -9,18 +10,23 @@ class PeliculaController extends Controller
     // Muestro todas las películas de la base de datos
     public function index()
     {
-        return view('peliculas/index');
+        $peliculas = Pelicula::all();
+
+        return view('peliculas/index', compact('peliculas'));
     }
     // Muestro sólo la película que coincida con el id solicitado
     public function show($id)
     {
-        return view('peliculas/show', compact('id'));
+        // Con findOrFail si no existe el id visualiza un error 404 
+        $pelicula = Pelicula::findOrFail($id);
+
+        return view('peliculas/show', compact('pelicula'));
     }
 
     // Retono la vista del formulario para añadir una película
     public function create()
     {
-        return view('peliculas/create');
+        return view('peliculas/create'); 
     }
     // La acción del formulario de añadir película redirige a este 
     // método para guardar los datos en la base de datos

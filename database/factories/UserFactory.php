03bb1acdb2dcc07042416c78a848790d9a88f5a7
RubenFern <rubenferval@hotmail.com>
2021-03-25 20:46:57 +0100

    Añadido hash para encriptar la contraseña del usuario. Campo role_id añadido con referencia a role de usuario

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -26,7 +28,8 @@ class UserFactory extends Factory
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('naranco'),
+            'role_id' => Role::getRoleByName('usuario')->first()->id,
             'remember_token' => Str::random(10),
         ];
     }

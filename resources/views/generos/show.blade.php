9e5344736efa9ba0819da8ce1993bc0a5d4d9f5c
RubenFern <rubenferval@hotmail.com>
2021-03-26 14:43:19 +0100

    Añadido formulario para eliminar el genero seleccionado

--- resources/views/generos/show.blade.php
+++ resources/views/generos/show.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
+
     <h2>Visualizo el genero con el id <?php echo $genero->id; ?></h2>
 
     <table border="1">
@@ -14,8 +9,16 @@
             <th>Género</th>
         </tr>
         <tr>
-            <td>{{ $genero->genero }}</td> 
+            <td>{{ $genero->genero }}</td>
+            <td>
+                <form method='POST' action="{{ route('generos.destroy', ['genero' => $genero->id])}}">
+                    @csrf
+                    @method('DELETE')
+
+                    <input type="submit" value="Eliminar género">
+                </form>
+            </td>
         </tr>
     </table>
-</body>
-</html>
+    
+@endsection

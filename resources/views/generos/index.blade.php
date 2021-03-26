01d2007626dc6de3ab9e6120bb06de9f081580d8
RubenFern <rubenferval@hotmail.com>
2021-03-26 14:42:37 +0100

    Añadidas las url de los generos mediante route para mostar cada uno individualmente

--- resources/views/generos/index.blade.php
+++ resources/views/generos/index.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
+
     <h2>Visualizo todos los generos</h2>
 
     <table border="1">
@@ -15,9 +10,13 @@
         </tr>
         @foreach ($generos as $genero)
             <tr>
-                <td><a href="generos/{{ $genero->id }}">{{ $genero->genero }}</a></td> 
+                <td>
+                    <a href="{{ route('generos.show', ['genero' => $genero->id]) }}">
+                        {{ $genero->genero }}
+                    </a>
+                </td> 
             </tr>
         @endforeach
     </table>
-</body>
-</html>
+
+@endsection

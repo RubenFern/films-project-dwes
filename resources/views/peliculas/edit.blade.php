435673aad2226e3cace7ace058c7f5252ca2f09a
RubenFern <rubenferval@hotmail.com>
2021-03-26 22:09:49 +0100

    Añadido en todos los campos input el método old() para que en caso de que suceda un error al insertar o editar los registros de las películas, guarde los campos que estén bien

--- resources/views/peliculas/edit.blade.php
+++ resources/views/peliculas/edit.blade.php
@@ -10,46 +10,47 @@
         <br>
 
         <label for="titulo">Título: </label>
-        <input type="text" name="titulo" value="{{ $pelicula->titulo }}" required>
+        <input type="text" name="titulo" value="{{ old('titulo') ?? $pelicula->titulo }}" required> 
+        {{-- La doble interrogación comrpueba si alguno de los 2 está definido, da prioridad al old() --}}
 
         <br><br>
 
         <label for="genero">Género: </label>
         <select name="id_genero" required>
             @foreach ($generos as $genero)
-                <option value="{{ $genero->id }}" {{ $genero->id == $pelicula->id_genero ? 'selected' : '' }}>{{ $genero->genero }}</option>
+                <option value="{{ $genero->id }}" {{ old('id_genero') == $pelicula->id_genero ? 'selected' : ($genero->id == $pelicula->id_genero ? 'selected' : '') }}>{{ $genero->genero }}</option>
             @endforeach
         </select>
 
         <br><br>
 
         <label for="sinopsis">Sinposis: </label><br>
-        <textarea name="sinopsis" cols="30" rows="10" required>{{ $pelicula->sinopsis }}</textarea>
+        <textarea name="sinopsis" cols="30" rows="10" required>{{ old('sinopsis') ?? $pelicula->sinopsis }}</textarea>
 
         <br><br>
 
         <label for="director">Director: </label>
-        <input type="text" name="director" value="{{ $pelicula->director }}" required>
+        <input type="text" name="director" value="{{ old('director') ?? $pelicula->director }}" required>
 
         <br><br>
 
         <label for="cantidad">Cantidad</label>
-        <input type="number" name="cantidad" min="0" value="{{ $pelicula->cantidad }}" required>
+        <input type="number" name="cantidad" min="0" value="{{ old('cantidad') ?? $pelicula->cantidad }}" required>
 
         <br><br>
 
         <label for="precio">Precio: </label>
-        <input type="number" name="precio" value="{{ $pelicula->precio }}" min="2.99" step="0.01" max="29.99" required>
+        <input type="number" name="precio" value="{{ old('precio') ?? $pelicula->precio }}" min="2.99" step="0.01" max="29.99" required>
 
         <br><br>
 
         <label for="año">Año: </label>
-        <input type="text" name="año" value="{{ $pelicula->año }}" pattern="^(19|20)\d{2}$" required>
+        <input type="text" name="año" value="{{ old('año') ?? $pelicula->año }}" pattern="^(19|20)\d{2}$" required>
 
         <br><br>
 
         <label for="imagen">Imagen: </label>
-        <input type="file" name="imagen" value="{{ $pelicula->imagen }}" required>
+        <input type="file" name="imagen" required>
 
         <br><br>
 

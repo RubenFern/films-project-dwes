24bd257982217b4f1413e8742a9f25028485ba82
RubenFern <rubenferval@hotmail.com>
2021-03-26 19:09:49 +0100

    Añadi un mesnaje de error flash que se muestra si el usuario intenta alquilar una pelicula que ya tiene alquilada

--- resources/views/peliculas-alquiladas/create.blade.php
+++ resources/views/peliculas-alquiladas/create.blade.php
@@ -4,4 +4,21 @@
 
     <h2>Quieres alquilar esta película??</h2>
     
+    <form method="POST" action="{{ route('peliculas-alquiladas.store', ['pelicula' => $idPelicula]) }}">
+        @csrf
+
+        <br>
+
+        <input type="submit" name="añadir" value="Sí">
+        <a href="{{ URL::previous() }}">No</a>
+
+        {{-- 
+        Muestro un mensaje en caso de alquilar una película y que el usuario ya la tenga alquilada. 
+        El error es flash, por lo que al recargar la página se elimina el error.
+        --}}
+        @if (session()->has('ya_alquilada'))
+            <p>{{ session()->get('ya_alquilada') }}</p>
+        @endif
+    </form>
+
 @endsection

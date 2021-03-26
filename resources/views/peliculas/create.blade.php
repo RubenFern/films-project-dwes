7a0ddab55de32ae1aaba6c747f905acf174e5952
RubenFern <rubenferval@hotmail.com>
2021-03-26 20:50:44 +0100

    Añadí el método old() en los values de los inputs del formulario para que cuando haya algún error en los campos, me guarde los que sí están bien

--- resources/views/peliculas/create.blade.php
+++ resources/views/peliculas/create.blade.php
@@ -10,46 +10,46 @@
         <br>
 
         <label for="titulo">Título: </label>
-        <input type="text" name="titulo" required>
+        <input type="text" name="titulo" value="{{ old('titulo') }}" required>
 
         <br><br>
 
         <label for="genero">Género: </label>
-        <select name="id_genero" required>
+        <select name="id_genero" >
             @foreach ($generos as $genero)
-                <option value="{{ $genero->id }}">{{ $genero->genero }}</option>
+                <option value="{{ $genero->id }}" {{ old('id_genero') == $genero->id ? 'selected' : '' }} required>{{ $genero->genero }}</option>
             @endforeach
         </select>
 
         <br><br>
 
-        <label for="sinopsis">Sinposis: </label><br>
-        <textarea name="sinopsis" cols="30" rows="10" required></textarea>
+        <label for="sinopsis">Sinopsis: </label><br>
+        <textarea name="sinopsis" cols="30" rows="10" required>{{ old('sinopsis') }}</textarea>
 
         <br><br>
 
         <label for="director">Director: </label>
-        <input type="text" name="director" required>
+        <input type="text" name="director" value="{{ old('director') }}" required>
 
         <br><br>
 
         <label for="cantidad">Cantidad</label>
-        <input type="number" name="cantidad" min="0" required>
+        <input type="number" name="cantidad" min="1" value="{{ old('cantidad') }}" required>
 
         <br><br>
 
         <label for="precio">Precio: </label>
-        <input type="number" name="precio" min="2.99" step="0.01" max="29.99" required>
+        <input type="number" name="precio" min="2.99" step="0.01" max="29.99" value="{{ old('precio') }}" required>
 
         <br><br>
 
         <label for="año">Año: </label>
-        <input type="text" name="año" pattern="^(19|20)\d{2}$" required>
+        <input type="text" name="año" pattern="^(19|20)\d{2}$" value="{{ old('año') }}" required>
 
         <br><br>
 
         <label for="imagen">Imagen: </label>
-        <input type="file" name="imagen" required>
+        <input type="file" name="imagen">
 
         <br><br>
 

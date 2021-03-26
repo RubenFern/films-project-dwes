6f09d53c3a42d16731692590b8712f399da7242e
RubenFern <rubenferval@hotmail.com>
2021-03-26 19:10:39 +0100

    Estructura empty añadida

--- resources/views/peliculas-alquiladas/index.blade.php
+++ resources/views/peliculas-alquiladas/index.blade.php
@@ -1,13 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-
-    @forelse ($peliculasAlquiladas as $peliculaAlquilada)
-        <h2>El usuario visualiza las peliculas alquiladas</h2>
-    @empty
+        
+    @empty ($peliculasAlquiladas)
         <h2>No tienes ninguna película alquilada</h2>
 
-        <p><a href="">Echa un vistazo a nuestras películas</a></p>
-    @endforelse
+        <p><a href="{{ route('peliculas.index') }}">Echa un vistazo a nuestras películas</a></p>
+    @else
+        <tr>
+            <th>Título</th> <th>Sinopsis</th> <th>Director</th> <th>Año</th> <th>imagen</th>
+        </tr>
+        @foreach ($peliculasAlquiladas as $peliculaAlquilada)
+            <tr>
+                <td>
+                    <a href="{{ route('peliculas.show', ['pelicula' => $peliculaAlquilada->id]) }}">
+                        {{ $peliculaAlquilada->titulo }}
+                    </a>
+                </td> 
+                <td>{{ $peliculaAlquilada->sinopsis }}</td> 
+                <td>{{ $peliculaAlquilada->director }}</td> 
+                <td>{{ $peliculaAlquilada->año }}</td> 
+                <td>
+                    <img width="50" src="{{ $peliculaAlquilada->imagen }}" >
+                </td>
+            </tr>
+        @endforeach
+    @endempty
 
 @endsection

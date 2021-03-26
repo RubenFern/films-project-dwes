445660760ef0caa9b29b8e1b3c179e00641be382
RubenFern <rubenferval@hotmail.com>
2021-03-26 14:47:30 +0100

    Se han creado los archivos para después añadir los formularios del CRUD

--- resources/views/peliculas-alquiladas/index.blade.php
+++ resources/views/peliculas-alquiladas/index.blade.php
@@ -1,20 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
 
-    @if ($peliculasAlquiladas)
+@section('content')
+
+    @forelse ($peliculasAlquiladas as $peliculaAlquilada)
+        <h2>El usuario visualiza las peliculas alquiladas</h2>
+    @empty
         <h2>No tienes ninguna película alquilada</h2>
 
-        <p><a href="peliculas">Echa un vistazo a nuestras películas</a></p>
-    @else
-        <h2>El usuario visualiza las peliculas alquiladas</h2>
-    @endif
+        <p><a href="">Echa un vistazo a nuestras películas</a></p>
+    @endforelse
 
-</body>
-</html>
+@endsection

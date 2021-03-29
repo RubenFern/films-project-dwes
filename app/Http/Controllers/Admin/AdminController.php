2a69b561689704b367f8ed5461f9d983a195e6bd
RubenFern <rubenferval@hotmail.com>
2021-03-29 22:57:38 +0200

    Ahora se retorna la vista con todos los productos y todos los géneros

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Genero;
+use App\Models\Pelicula;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin');
+        $peliculas = Pelicula::all();
+        $generos = Genero::all();
+
+        return view('admin', compact('peliculas', 'generos'));
     }
 }

c7b5cf64a700b87c706a44d014a31d3bb2191196
RubenFern <rubenferval@hotmail.com>
2021-03-31 22:04:57 +0200

    Mejora en la vista de los géneros

--- resources/views/generos/index.blade.php
+++ resources/views/generos/index.blade.php
@@ -2,21 +2,18 @@
 
 @section('content')
 
-    <h2>Visualizo todos los generos</h2>
+    <section class="max-w-sm md:max-w-lg lg:max-w-3xl xl:max-w-5xl m-auto mb-6">
+        <h1 class="my-10 text-teal-300 ml-4 md:ml-0 text-2xl md:text-3xl font-semibold">Elige el género que quieras</h1>
 
-    <table border="1">
-        <tr>
-            <th>Género</th>
-        </tr>
-        @foreach ($generos as $genero)
-            <tr>
-                <td>
-                    <a href="{{ route('generos.show', ['genero' => $genero->id]) }}">
-                        {{ $genero->genero }}
-                    </a>
-                </td> 
-            </tr>
-        @endforeach
-    </table>
+        <div class="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 xl:gap-10">
+            @foreach ($generos as $genero)
+                <a class="text-gray-300 text-center hover:bg-teal-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="{{ route('generos.show', ['genero' => $genero->id]) }}">
+                    {{ $genero->genero }}
+                </a>
+            @endforeach
+        </div>
+        
+
+    </section>
 
 @endsection
--- resources/views/generos/show.blade.php
+++ resources/views/generos/show.blade.php
@@ -2,23 +2,33 @@
 
 @section('content')
 
-    <h2>Visualizo el genero con el id <?php echo $genero->id; ?></h2>
+    <section class="max-w-sm lg:max-w-5xl m-auto mb-6">
+        <h1 class="my-8 text-teal-300 text-3xl font-semibold">Películas del género de {{ $genero->genero }}</h1>
 
-    <table border="1">
-        <tr>
-            <th>Género</th>
-        </tr>
-        <tr>
-            <td>{{ $genero->genero }}</td>
-            <td>
-                <form method='POST' action="{{ route('generos.destroy', ['genero' => $genero->id])}}">
-                    @csrf
-                    @method('DELETE')
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
+            @forelse ($peliculasFiltradas as $pelicula)
+                <div class="flex bg-black bg-opacity-50 rounded-2xl box-border w-full">
+                    <img class="rounded-l-xl w-32 lg:w-40" src="{{ $pelicula->imagen }}" alt="{{ $pelicula->imagen }}">
+                    <div class="flex flex-col justify-between box-border w-full">
+                        <div>
+                            <h2 class="text-teal-100 text-xl p-4 pl-10">{{ $pelicula->titulo }}</h2>
+                            <h4 class="text-gray-600 pl-10">Año: <span class="text-white">{{ $pelicula->año }}</span></h4>
+                            <h4 class="text-gray-600 pl-10">Género: <span class="text-white">{{ $pelicula->genero->genero }}</span></h4>
+                        </div>         
+                        
+                        <div class="pl-4 mb-7 m-auto">
+                            <a class="bg-teal-500 py-2 px-16 rounded-xl" href="{{ route('peliculas.show', ['pelicula' => $pelicula->id]) }}">Ver película</a>
+                        </div>
+                    </div>
+                </div>
 
-                    <input type="submit" value="Eliminar género">
-                </form>
-            </td>
-        </tr>
-    </table>
+            @empty
+                <div class="rounded-2xl films_not_found flex items-end justify-center">
+                    <h1 class="text-5xl font-semibold p-5 text-center mb-1">No tenemos películas que coinicidan con ese género</h1>
+                </div>
+            @endforelse
+        </div>
+        
+    </section>
     
 @endsection

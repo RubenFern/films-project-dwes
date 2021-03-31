38e967cc7c0918868f19f36fb4973be8295c8776
RubenFern <rubenferval@hotmail.com>
2021-03-31 22:05:26 +0200

    Mejora en la interfaz de confirmación

--- resources/views/peliculas-alquiladas/create.blade.php
+++ resources/views/peliculas-alquiladas/create.blade.php
@@ -3,23 +3,22 @@
 @section('content')
 
     <section class="w-4/6 m-auto">
-        <h2 class="text-teal-500 text-3xl font-semibold">¿Quieres alquilar la pleícula de <span class="text-teal-200">{{ $PeliculaAlquilada->titulo }}</span></h2>
+        <h2 class="text-teal-500 text-center text-3xl font-semibold mt-20 mb-5">¿Quieres alquilar la película de <span class="text-white">{{ $PeliculaAlquilada->titulo }}</span>?</h2>
         
         <form method="POST" action="{{ route('peliculas-alquiladas.store', ['pelicula' => $PeliculaAlquilada]) }}">
             @csrf
 
             <br>
 
-            <div class="flex justify-center mt-20">
-                <button class="mr-5 bg-teal-600 hover:bg-teal-700 text-white font-bold py-2 px-6 rounded-lg" type="submit" name="añadir">
+            <div class="flex justify-center">
+                <button class="text-gray-300 bg-gray-700 hover:text-white px-5 py-2 rounded-md text-lg font-medium mr-2" type="submit" name="añadir">
                     Sí
                 </button>
 
-                <a class="mr-5 bg-teal-600 hover:bg-teal-700 text-white font-bold py-1.5 px-6 rounded-lg" href="{{ URL::previous() }}">No</a>  
+                <a class="text-gray-300 bg-gray-700 hover:text-white px-5 py-2 rounded-md text-lg font-medium ml-2" href="{{ URL::previous() }}">No</a>  
             </div>
                
         </form>
     </section>
-    
 
 @endsection

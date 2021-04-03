01b2ec5acdeb97350d480abf9de875bdf4cdef85
RubenFern <rubenferval@hotmail.com>
2021-04-03 22:05:19 +0200

    Si el usuario está logueado muestro siempre su home respectivamente

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MainController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        if (Auth::user())
+        {
+            // En el controlador de home compruebo si es admin o el usuario para mostarles su respectivo home
+            return redirect()->route('home');
+        } else
+        {
+            return view('welcome');
+        }
+        
     }
 }
